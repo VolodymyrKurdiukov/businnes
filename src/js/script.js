bbc32bd41484a26a4a228366645a3f9f2cd5e2f4
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-05-27 09:44:59 +0300

    header_final

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,25 @@
-const hamburber = document.querySelector('.hamburger'),
-	menu = document.querySelector('.header-menu'),
-	closeElem = document.querySelector('.header-menu__close');
+// const hamburber = document.querySelector('.hamburger'),
+// 	menu = document.querySelector('.header'),
+// 	closeElem = document.querySelector('.header-menu__close');
 
-hamburber.addEventListener('click', () => {
-	menu.classList.add('active');
-});
+// hamburber.addEventListener('click', () => {
+// 	menu.classList.add('active');
+// });
 
-closeElem.addEventListener('click', () => {
-	menu.classList.remove('active');
+// closeElem.addEventListener('click', () => {
+// 	menu.classList.remove('active');
+// });
+
+$('.nav-header__icon').click(function (event) {
+	$(this).toggleClass('active');
+	$('.nav-header__menu').toggleClass('active');
+	if ($(this).hasClass('active')) {
+		$('body').data('scroll', $(window).scrollTop());
+	}
+	$('body').toggleClass('lock');
+	if (!$(this).hasClass('active')) {
+		$('body,html').scrollTop(parseInt($('body').data('scroll')));
+	}
 });
 
 function ibg() {
